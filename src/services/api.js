d86066ebd451460b4f809319910607bb7cad4df3
BHUVI1529 <srinivasanbhuvana7@gmail.com>
2025-04-24 15:19:22 +0530

    adding credentials true

--- src/services/api.js
+++ src/services/api.js
@@ -7,6 +7,7 @@ const api = axios.create({
     headers: {
        'Content-Type': 'application/json',
    },
+   withCredentials: true  // ✅ important for cookies/session/cors
 });
 
 export default api;
